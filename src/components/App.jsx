bbca2f6eb1ba22a521f711948ee75c0ca28d2e47
Torie-Russell <75646749+Torie-Russell@users.noreply.github.com>
2023-08-08 20:10:41 -0400

    changes to App and Homepage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,6 @@ import {useState} from "react";
 import Login from "./Login/Login";
 import Homepage from "./Homepage";
 
-import Header from "./Header/Header";
-import Sidebar from "./Sidebar/Sidebar";
-import Body from "./Body/Body";
-import Home from "./Body/Home";
-import Error from "./Error/Error";
-import Live from "./Live/Live";
-import VideoShowcase from "./Body/Showcase/VideoShowcase";
-import SearchResults from "./Body/SearchResults/SearchResults";
-import Questions from "./Body/Questions/Questions";
-
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
@@ -25,37 +15,3 @@ export default function App() {
     </>
   );
 }
-
-
-const appRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <Body />,
-    errorElement: <Error />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-      },
-      {
-        path: "/watch",
-        element: <VideoShowcase />,
-      },
-    ],
-  },
-  {
-    path: "/results",
-    errorElement: <Error />,
-    element: <SearchResults />,
-  },
-  {
-    path: "/live",
-    errorElement: <Error />,
-    element: <Live />,
-  },
-  {
-    path: "/questions",
-    errorElement: <Error />,
-    element: <Questions />,
-  },
-]);

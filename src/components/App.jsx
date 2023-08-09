9856934bc14ab7ee18b3c9386d9d095d1b3f68e2
Torie-Russell <75646749+Torie-Russell@users.noreply.github.com>
2023-08-08 20:31:16 -0400

    added Login page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,13 +6,6 @@ import Homepage from "./Homepage";
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
-    <div className="hidden md:block bg-black  text-white font-primary cursor-default">
-      <Header />
-      <div className="min-h-screen flex items-start relative">
-        <Sidebar />
-        <RouterProvider router={appRouter} />
-      </div>
-    </div>
     <>
     {isLoggedIn ?(
       <Homepage />
